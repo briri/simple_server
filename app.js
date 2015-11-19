bb2acda1deece8cfa58eac39a70b816beaae9d00
briley <brian.riley@ucop.edu>
2015-11-19 09:31:37 -0800

    Removed erroneous require statement

--- app.js
+++ app.js
@@ -13,15 +13,11 @@ server.get('/', function(req, res){
 	res.status(200).send("You found it!");
 });
 
-server.on('close', function(){
-	LOG.info(CONFIG['server']['name'] + " no longer listening on port " + CONFIG['server']['port']);
-})
-
 server.on('error', function(err){
 	LOG.error(CONFIG['server']['name'] + " has encountered an error: ", err);
 });
 
 // Startup the web server	
-server.listen(CONFIG['server']['port'], function(){
-	LOG.info(CONFIG['server']['name'] + " listening on port " + CONFIG['server']['port']);
+server.listen(18880, function(){
+	
 });
